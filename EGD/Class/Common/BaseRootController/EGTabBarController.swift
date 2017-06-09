0f55eed53b078e051de9774f639f4b8a94937b42
woaidankea <jieku@jiekudeMacBook-Pro.local>
2017-06-10 00:36:12 +0800

    添加中间tabbar

--- EGD/Class/Common/BaseRootController/EGTabBarController.swift
+++ EGD/Class/Common/BaseRootController/EGTabBarController.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class EGTabBarController: UITabBarController ,UITabBarControllerDelegate{
     
+    fileprivate lazy var addBtn:UIButton = {
+        var btn = UIButton.init(type: UIButtonType.custom)
+        let width = kMainBoundsWidth/5
+        btn.frame = CGRect(x: 2*width, y: 0, width: width, height: 49)
+        btn .setBackgroundImage(UIImage(named: "tabbar_compose_button"), for: .normal)
+        btn.setBackgroundImage(UIImage(named: "tabbar_compose_button_highlighted"), for: .highlighted)
+        btn.setImage(UIImage(named: "tabbar_compose_icon_add"), for: UIControlState.normal)
+        btn.setImage(UIImage(named: "tabbar_compose_icon_add_highlighted"), for: UIControlState.highlighted)
+        btn.addTarget(self, action: #selector(addClick), for: .touchUpInside)
+        return btn
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -17,15 +29,26 @@ class EGTabBarController: UITabBarController ,UITabBarControllerDelegate{
         tabBarBackgroud()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tabBar.addSubview(addBtn)
+    }
+    //点击拍视频
+   @objc fileprivate func addClick() {
+    let statuscontroller = EGStatusViewController()
+    let customVc = EGNavigationController(rootViewController:statuscontroller)
+    present(customVc, animated: true, completion: nil)
+    }
+    
     //添加TabBar背景颜色
-    func tabBarBackgroud() {
+   fileprivate func tabBarBackgroud() {
         let backView = UIView(frame: CGRect(x: 0, y: 0, width: kMainBoundsWidth, height: 49))
         backView.backgroundColor = UIColor.white
         tabBar.insertSubview(backView, at: 0)
         tabBar.isOpaque = true
     }
     
-    func addChildViewControllers() {
+   fileprivate func addChildViewControllers() {
         setupOneChildViewController("陪你", image: "home_tabbar_22x22_", selectedImage: "home_tabbar_press_22x22_", controller: EGEmergencyViewController.init())
          setupOneChildViewController("视频", image: "video_tabbar_22x22_", selectedImage: "video_tabbar_press_22x22_", controller: EGVideoViewController.init())
          setupOneChildViewController("直播", image: "newcare_tabbar_22x22_", selectedImage: "newcare_tabbar_press_22x22_", controller: EGLivingShowViewController.init())
